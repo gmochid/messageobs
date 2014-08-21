2eb29752fcdb58bab08f61d9c29d10afb56f8b14
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2014-08-21 22:24:44 +0700

    update database

--- messageobs/settings.py
+++ messageobs/settings.py
@@ -8,10 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -73,17 +69,8 @@ WSGI_APPLICATION = 'messageobs.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'observer2',
-        'USER': 'root',
-        'PASSWORD': 'seagate',
-        'HOST': '127.0.0.1',   
-        'PORT': '3306',
-    }
-}
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
